4e8de780b4af9f72af2698c807d9b4b13b57a871
asm2212 <asmareadmasu0@gmail.com>
2024-12-13 12:30:17 +0300

    add interactive line chart with gradient styling and tooltips

--- lib/screen/sleep_tracker/sleep_tracker_screen.dart
+++ lib/screen/sleep_tracker/sleep_tracker_screen.dart
@@ -1,4 +1,5 @@
 import 'package:fit_app/common/color_extension.dart';
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 
 class SleepTrackerScreen extends StatefulWidget {
@@ -40,40 +41,14 @@ class _SleepTrackerScreenState extends State<SleepTrackerScreen> {
     var media = MediaQuery.of(context).size;
     // final tooltipsOnBar = lineBarsData1[0];
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: ColorExtension.white,
-        centerTitle: true,
-        elevation: 0,
-        leading: InkWell(
-          onTap: () {
-            Navigator.pop(context);
-          },
-          child: Container(
-            margin: const EdgeInsets.all(8),
-            height: 40,
-            width: 40,
-            alignment: Alignment.center,
-            decoration: BoxDecoration(
-                color: ColorExtension.lightGray,
-                borderRadius: BorderRadius.circular(10)),
-            child: Image.asset(
-              "assets/images/black_btn.png",
-              width: 15,
-              height: 15,
-              fit: BoxFit.contain,
-            ),
-          ),
-        ),
-        title: Text(
-          "Sleep Tracker",
-          style: TextStyle(
-              color: ColorExtension.black,
-              fontSize: 19,
-              fontWeight: FontWeight.w700),
-        ),
-        actions: [
-          InkWell(
-            onTap: () {},
+        appBar: AppBar(
+          backgroundColor: ColorExtension.white,
+          centerTitle: true,
+          elevation: 0,
+          leading: InkWell(
+            onTap: () {
+              Navigator.pop(context);
+            },
             child: Container(
               margin: const EdgeInsets.all(8),
               height: 40,
@@ -83,15 +58,292 @@ class _SleepTrackerScreenState extends State<SleepTrackerScreen> {
                   color: ColorExtension.lightGray,
                   borderRadius: BorderRadius.circular(10)),
               child: Image.asset(
-                "assets/images/more_btn.png",
+                "assets/images/black_btn.png",
                 width: 15,
                 height: 15,
                 fit: BoxFit.contain,
               ),
             ),
           ),
+          title: Text(
+            "Sleep Tracker",
+            style: TextStyle(
+                color: ColorExtension.black,
+                fontSize: 19,
+                fontWeight: FontWeight.w700),
+          ),
+          actions: [
+            InkWell(
+              onTap: () {},
+              child: Container(
+                margin: const EdgeInsets.all(8),
+                height: 40,
+                width: 40,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                    color: ColorExtension.lightGray,
+                    borderRadius: BorderRadius.circular(10)),
+                child: Image.asset(
+                  "assets/images/more_btn.png",
+                  width: 15,
+                  height: 15,
+                  fit: BoxFit.contain,
+                ),
+              ),
+            ),
+          ],
+        ),
+        backgroundColor: ColorExtension.white,
+        body: SingleChildScrollView(
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                        padding: const EdgeInsets.only(left: 15),
+                        height: media.width * 0.5,
+                        width: double.maxFinite,
+                        child: LineChart(
+                          LineChartData(
+                            // showingTooltipIndicators:
+                            //     showingTooltipOnSpots.map((index) {
+                            //   return ShowingTooltipIndicators([
+                            //     LineBarSpot(
+                            //       tooltipsOnBar,
+                            //       lineBarsData1.indexOf(tooltipsOnBar),
+                            //       tooltipsOnBar.spots[index],
+                            //     ),
+                            //   ]);
+                            // }).toList(),
+                            lineTouchData: LineTouchData(
+                              enabled: true,
+                              handleBuiltInTouches: false,
+                              touchCallback: (FlTouchEvent event,
+                                  LineTouchResponse? response) {
+                                if (response == null ||
+                                    response.lineBarSpots == null) {
+                                  return;
+                                }
+                                if (event is FlTapUpEvent) {
+                                  final spotIndex =
+                                      response.lineBarSpots!.first.spotIndex;
+                                  showingTooltipOnSpots.clear();
+                                  setState(() {
+                                    showingTooltipOnSpots.add(spotIndex);
+                                  });
+                                }
+                              },
+                              mouseCursorResolver: (FlTouchEvent event,
+                                  LineTouchResponse? response) {
+                                if (response == null ||
+                                    response.lineBarSpots == null) {
+                                  return SystemMouseCursors.basic;
+                                }
+                                return SystemMouseCursors.click;
+                              },
+                              getTouchedSpotIndicator:
+                                  (LineChartBarData barData,
+                                      List<int> spotIndexes) {
+                                return spotIndexes.map((index) {
+                                  return TouchedSpotIndicatorData(
+                                    const FlLine(
+                                      color: Colors.transparent,
+                                    ),
+                                    FlDotData(
+                                      show: true,
+                                      getDotPainter:
+                                          (spot, percent, barData, index) =>
+                                              FlDotCirclePainter(
+                                        radius: 3,
+                                        color: Colors.white,
+                                        strokeWidth: 1,
+                                        strokeColor:
+                                            ColorExtension.primaryColor2,
+                                      ),
+                                    ),
+                                  );
+                                }).toList();
+                              },
+                              touchTooltipData: LineTouchTooltipData(
+                                getTooltipColor: (touchedSpot) =>
+                                    ColorExtension.secondaryColor1,
+                                tooltipRoundedRadius: 5,
+                                getTooltipItems:
+                                    (List<LineBarSpot> lineBarsSpot) {
+                                  return lineBarsSpot.map((lineBarSpot) {
+                                    return LineTooltipItem(
+                                      "${lineBarSpot.y.toInt()} hours",
+                                      const TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 10,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    );
+                                  }).toList();
+                                },
+                              ),
+                            ),
+                            lineBarsData: lineBarsData1,
+                            minY: -0.01,
+                            maxY: 10.01,
+                            titlesData: FlTitlesData(
+                                show: true,
+                                leftTitles: const AxisTitles(),
+                                topTitles: const AxisTitles(),
+                                bottomTitles: AxisTitles(
+                                  sideTitles: bottomTitles,
+                                ),
+                                rightTitles: AxisTitles(
+                                  sideTitles: rightTitles,
+                                )),
+                            gridData: FlGridData(
+                              show: true,
+                              drawHorizontalLine: true,
+                              horizontalInterval: 2,
+                              drawVerticalLine: false,
+                              getDrawingHorizontalLine: (value) {
+                                return FlLine(
+                                  color: ColorExtension.gray.withOpacity(0.15),
+                                  strokeWidth: 2,
+                                );
+                              },
+                            ),
+                            borderData: FlBorderData(
+                              show: true,
+                              border: Border.all(
+                                color: Colors.transparent,
+                              ),
+                            ),
+                          ),
+                        ),
+                        ),
+                  ]
+                  ),
+            ),
+          ]),
+        ),
+        );
+  }
+
+  List<LineChartBarData> get lineBarsData1 => [
+        lineChartBarData1_1,
+      ];
+
+  LineChartBarData get lineChartBarData1_1 => LineChartBarData(
+        isCurved: true,
+        gradient: LinearGradient(colors: [
+          ColorExtension.primaryColor2,
+          ColorExtension.primaryColor1,
+        ]),
+        barWidth: 2,
+        isStrokeCapRound: true,
+        dotData: const FlDotData(show: false),
+        belowBarData: BarAreaData(
+          show: true,
+          gradient: LinearGradient(colors: [
+            ColorExtension.primaryColor2,
+            ColorExtension.white,
+          ], begin: Alignment.topCenter, end: Alignment.bottomCenter),
+        ),
+        spots: const [
+          FlSpot(1, 3),
+          FlSpot(2, 5),
+          FlSpot(3, 4),
+          FlSpot(4, 7),
+          FlSpot(5, 4),
+          FlSpot(6, 8),
+          FlSpot(7, 5),
         ],
-      ),
+      );
+
+  SideTitles get rightTitles => SideTitles(
+        getTitlesWidget: rightTitleWidgets,
+        showTitles: true,
+        interval: 2,
+        reservedSize: 40,
+      );
+
+  Widget rightTitleWidgets(double value, TitleMeta meta) {
+    String text;
+    switch (value.toInt()) {
+      case 0:
+        text = '0h';
+        break;
+      case 2:
+        text = '2h';
+        break;
+      case 4:
+        text = '4h';
+        break;
+      case 6:
+        text = '6h';
+        break;
+      case 8:
+        text = '8h';
+        break;
+      case 10:
+        text = '10h';
+        break;
+      default:
+        return Container();
+    }
+
+    return Text(text,
+        style: TextStyle(
+          color: ColorExtension.gray,
+          fontSize: 14,
+          fontWeight: FontWeight.w500,
+        ),
+        textAlign: TextAlign.center);
+  }
+
+  SideTitles get bottomTitles => SideTitles(
+        showTitles: true,
+        reservedSize: 32,
+        interval: 1,
+        getTitlesWidget: bottomTitleWidgets,
+      );
+
+  Widget bottomTitleWidgets(double value, TitleMeta meta) {
+    var style = TextStyle(
+      color: ColorExtension.gray,
+      fontSize: 14,
+    );
+    Widget text;
+    switch (value.toInt()) {
+      case 1:
+        text = Text('Sun', style: style);
+        break;
+      case 2:
+        text = Text('Mon', style: style);
+        break;
+      case 3:
+        text = Text('Tue', style: style);
+        break;
+      case 4:
+        text = Text('Wed', style: style);
+        break;
+      case 5:
+        text = Text('Thu', style: style);
+        break;
+      case 6:
+        text = Text('Fri', style: style);
+        break;
+      case 7:
+        text = Text('Sat', style: style);
+        break;
+      default:
+        text = const Text('');
+        break;
+    }
+
+    return SideTitleWidget(
+      axisSide: meta.axisSide,
+      space: 10,
+      child: text,
     );
   }
 }
